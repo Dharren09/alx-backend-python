04ff2bce7de3d0692341205f51f6e46c1e06f952
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-02 18:27:32 +0000

    Async comprehensions

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """writes a coroutine that takes in no arg"""
 
+from typing import Generator
 import asyncio
-import random
+from random import uniform
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """coroutine will loop 10 times"""
-    for i in range(10):
+    for _ in range(10):
         """each time synchronously waits 1 sec"""
-        await asyncio.sleep(i)
+        await asyncio.sleep(1)
         """then yeilds a random number btn 0  and 10"""
-        yield random.randint(0, 10)
+        yield uniform(0, 10)
