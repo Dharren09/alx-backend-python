e2793d0bdf6fe8d4bb674a5314c32927a7226e3b
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-22 16:32:03 +0300

    Unittests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,10 +1,10 @@
-#!//usr/bin/python3
+#!//usr/bin/env python3
 """uses a function, plays with the python console and tests the method
 the body of the test method should not be longer than 2 lines"""
 
 import unittest
-from utils import access_nested_map
 from parameterized import parameterized
+from utils import access_nested_map
 from unittest.mock import patch, Mock
 from typing import Dict, Tuple, Union
 
@@ -18,7 +18,7 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map: Dict, path: Tuple,
                                expected: Union[int, str]):
-        self.assertEqual(access_nested_map(nested_map, path), nested_map[path])
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
 
 if __name__ == "__main__":
