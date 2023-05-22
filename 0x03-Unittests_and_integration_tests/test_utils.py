975443da6f34f2e82c9b9ef047411dad497cbfae
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-22 15:53:33 +0300

    unittests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 the body of the test method should not be longer than 2 lines"""
 
 import unittest
-from utils import access_nest_map
+from utils import access_nested_map
 from parameterized import parameterized
 from unittest.mock import patch, Mock
 from typing import Dict, Tuple, Union
@@ -12,9 +12,9 @@ from typing import Dict, Tuple, Union
 class TestAccessNestedMap(unittest.TestCase):
     """using a decorator to test the function for the following inputs"""
     @parameterized.expand([
-        ({"a": 1}, ("a",)),
-        ({"a": {"b": 2}}, ("a",)),
-        ({"a": {"b": 2}}, ("a", "b")),
+        ({'a': 1}, ('a',), 1),
+        ({'a': {'b': 2}}, ('a',), {'b': 2}),
+        ({'a': {'b': 2}}, ('a', 'b'), 2),
     ])
     def test_access_nested_map(self, nested_map: Dict, path: Tuple,
                                expected: Union[int, str]):
